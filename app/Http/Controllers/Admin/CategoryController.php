eadc053b9cf849c252709aecab0bfe6975f2676d
juanlara17 <juanlara17@hotmail.com>
2020-01-06 21:40:27 -0500

    Se configuro CategoryController INDEX para retornar todas las categorias y SHOW para verificar si estan disponibles o no

---
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Category;
+
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        /*$cat = new Category();
+        $cat->name = 'Niños';
+        $cat->slug = 'Niños';
+        $cat->description = 'Niños';
+        $cat->save();
+        return $cat;*/
+
+        return Category::all();
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($slug)
+    {
+        if (Category::where('slug', $slug)->first()) {
+            return 'Slug exite';
+        }else{
+            return 'Slug disponible';
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
